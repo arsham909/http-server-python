1e6f59c19ec919a85fe2f41bed00f3f8fcbf570f
arsham <arsham202@gmail.com>
2026-04-08 12:57:28 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,13 +5,17 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # TODO: Uncomment the code below to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
     with socket.create_server(("localhost",4221), reuse_port=True) as server:
+        server.listen()
         conncetion, address = server.accept()
-        conncetion.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        recv = conncetion.recv(1024)
+        
+        data = recv.decode("utf-8").split(' ')
+        if data[1] == "/":
+            conncetion.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        else:
+            conncetion.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        
         
         
 
