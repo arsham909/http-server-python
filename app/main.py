7d50cb945071a3d5d749838451ca112aeca46a33
arsham <arsham202@gmail.com>
2026-04-08 13:40:45 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,19 +6,25 @@ def main():
     print("Logs from your program will appear here!")
 
     with socket.create_server(("localhost",4221), reuse_port=True) as server:
-        server.listen()
-        conncetion, address = server.accept()
-        recv = conncetion.recv(1024)
-        
-        request = recv.decode().split('\r\n')
-        path = request[0].split(' ')
-        if path[1] == "/":
-            conncetion.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-        elif "/echo/" in path[1]:
-            conncetion.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 4\r\n\r\nabcd")
-        else:
-            conncetion.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        while True:
+            server.listen()
+            conncetion, address = server.accept()
+            recv = conncetion.recv(1024)
             
+            request = recv.decode().split('\r\n')
+            path = request[0].split(' ')[1]
+            paths = path.split("/")            
+            if path == "/":
+                conncetion.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+                break
+            elif paths[1] == "echo":
+                echo = paths[2]
+                data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo)}\r\n\r\n{echo}"
+                conncetion.sendall(data.encode())
+            else:
+                conncetion.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                break
+                
         
         
 
