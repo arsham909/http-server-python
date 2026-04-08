8b954cf85023dd745ff8e2a8e499b42de2146235
arsham <arsham202@gmail.com>
2026-04-08 12:31:04 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,8 +7,15 @@ def main():
 
     # TODO: Uncomment the code below to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    # server_socket.accept() # wait for client
+    with socket.create_server(("localhost",8000), reuse_port=True) as server:
+        conncetion, address = server.accept()
+        # print(f"conncetion is working {conncetion}")
+        
+        conncetion.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        
+        
 
 
 if __name__ == "__main__":
