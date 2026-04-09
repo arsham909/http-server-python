0bbb6f292fedf3e4f03b3b88ef676235bbf53100
arsham <arsham202@gmail.com>
2026-04-09 11:53:18 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,38 +1,70 @@
-import socket  # noqa: F401
+import socket  
+from threading import Thread
 
+class server_side():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.running = True
+        self.start_server()
+    
+    def start_server(self):
+        with socket.create_server((self.host, self.port), reuse_port=True) as server:
+            print(f"Server is open on port {self.port}")
+            while self.running:
+                connection, address = server.accept()
+                # To make it concurrent, we spawn a thread for each client
+                thread = Thread(target=self.handle_client, args=(connection,))
+                thread.start()     
+    
+    def handle_client(self, connection):
+        with connection :
+            try:
+                recv = connection.recv(1024)
+                if not recv:
+                    return
+                respond = self.user_request(recv)
+                connection.sendall(respond)
+                connection.settimeout(10.0)
+          
+            except Exception as e:
+                print(e)
+             
+        
+    def user_request(self, recv):
+        request = recv.decode().split('\r\n')
+        # parsing safety
+        if not request: return b"HTTP/1.1 400 Bad Request\r\n\r\n"
+        
+        path = request[0].split(' ')[1]
+        paths = path.split("/")
+        for line in request:
+            if line.startswith("User-Agent: "):
+                user_agent = line.split("User-Agent: ")[1]
+        
+        if paths[1] == "echo":
+            echo = paths[2]
+            data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo)}\r\n\r\n{echo}"
+            return data.encode()
+        elif paths[1] == "user-agent":
+                data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+                return data.encode()
+        elif path == "/":
+            return  b"HTTP/1.1 200 OK\r\n\r\n"
+        elif paths[1] == "quit":
+            self.running =  False
+            return b"HTTP/1.1 200 OK\r\n\r\n"
+        
+        else: 
+            return b"HTTP/1.1 404 Not Found\r\n\r\n"
+            
+        
+               
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    with socket.create_server(("localhost",4221), reuse_port=True) as server:
-        while True:
-            server.listen()
-            conncetion, address = server.accept()
-            recv = conncetion.recv(1024)
-            
-            request = recv.decode().split('\r\n')
-            path = request[0].split(' ')[1]
-            paths = path.split("/")
-            user_agent = request[2]
-            if path == "/":
-                conncetion.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-                break
-            elif paths[1] == "echo":
-                echo = paths[2]
-                data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo)}\r\n\r\n{echo}"
-                conncetion.sendall(data.encode())
-            elif paths[1] == "user-agent":
-                data = user_agent.split("User-Agent: ")
-                data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(data[1])}\r\n\r\n{data[1]}"
-                conncetion.sendall(data.encode())
-            else:
-                conncetion.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-                break
-                
-        
-        
-
+    
+    server_side("localhost", 4221)
+     
 
 if __name__ == "__main__":
     main()
