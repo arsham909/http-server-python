d92a386c5ae8389856b0b1edb9fba4ba03ea94f1
arsham <arsham202@gmail.com>
2026-04-09 14:01:48 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 import socket  
 from threading import Thread
+import sys, pathlib
+
 
 class server_side():
-    def __init__(self, host, port):
+    def __init__(self, host, port, dir=None):
         self.host = host
         self.port = port
         self.running = True
+        self.dir = dir
+        if dir:
+            self.dir = dir.split("/")[1]
+            pathlib.Path(self.dir).mkdir(exist_ok=True, parents=True)
         self.start_server()
     
     def start_server(self):
@@ -47,14 +53,19 @@ class server_side():
             data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo)}\r\n\r\n{echo}"
             return data.encode()
         elif paths[1] == "user-agent":
-                data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-                return data.encode()
+            data = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+            return data.encode()
         elif path == "/":
             return  b"HTTP/1.1 200 OK\r\n\r\n"
         elif paths[1] == "quit":
             self.running =  False
             return b"HTTP/1.1 200 OK\r\n\r\n"
-        
+        elif paths[1] == "files":
+            echo = paths[2]
+            with open(f"/{self.dir}/{echo}", 'r') as file:
+                content = file.read()
+                data = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(content)}\r\n\r\n{content}"
+                return data.encode()
         else: 
             return b"HTTP/1.1 404 Not Found\r\n\r\n"
             
@@ -62,8 +73,10 @@ class server_side():
                
 
 def main():
-    
-    server_side("localhost", 4221)
+    if sys.argv[1] == "--directory" or sys.argv[1] == "-d":
+        server_side("localhost", 4221, sys.argv[2])
+    else:
+        server_side("localhost", 4221)
      
 
 if __name__ == "__main__":
